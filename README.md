b248be8eaa350e4262673930586879f67baae98d
Laddge <dev.laddge@gmail.com>
2021-08-31 11:03:54 +0900

    [feat] created module

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # magisk-custom-sounds
 customize system sounds
+
+## Usage
+You can use this module to replace files in ```/system/media/audio/ui/``` or ```/product/media/audio/ui/```.
+
+1. Put your sound files to ```/sdcard/UISounds/```.
+
+2. Install this module with Magisk.
+
+3. If you have updated your sound files, please reinstall this module with Magisk.
+
+## Tips
+You can disable the sound by placing empty files.
+
+ex. You can disable screenshots and camera shutter sounds by making the following sound files empty.
+
+```
+audio_end.ogg
+audio_initiate.ogg
+camera_click.ogg
+camera_focus.ogg
+VideoRecord.ogg
+VideoStop.ogg
+```
+
+## Changelog
+
+### v1
+Initial version
+
+## Licence
+This module is under the MIT-License.
+
+See also [LICENSE](LICENSE).
+
+## Author
+Laddge
---
+++ customize.sh
@@ -0,0 +1,54 @@
+#!/system/bin/sh
+ui_print "llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllllllllllllll........lllllllllllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllllllllll:.       .. .cllllllllllllllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllllllllll:              ;lllllllllllllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllllllllll:  .     ;.    ;lllllllllllllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllllllllll: 'lKo xk.lK   ;lllllllllllllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllllllllll: .'Xk.0K.xW   ;lllllllllllllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllllllllll: c0OO0KK000   :ollllllllllllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllllllllll: 'ckO0dokdo .;..llllllllllllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllllllllll: :Ooodod0KX..   :cllllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllllllll;,;kWKKKXNMMMMx    .,lllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllllll:. .MMMWWWMMMMMM0,     ';lllllllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllllll.  ckNWMWWMMMMWNXKK.     .lllllllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllllll  .0MMMMMMMMMMMMMWWWK  .   ;lllllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllll;'.cWMMMMMMMMMMMMMMMMMX      ;lllllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllll.  ;MMMMMMMMMMMMMMMMMMX      ..llllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllc   :xMMMMMMMMMMMMMMMMMMX.       llllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllllllc...d0MMMMMMMMMMMMMMMMMWK       .llllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllodkOO::cXMMMMMMMMMMMMMMMXk.    .;lllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllldxkkkOOOOl..:MMMMMMMMMMMMWXOxo'  .lxkllllllllllllllllllllllll"
+ui_print "lllllllllllllllllllllkkOOOOOOOOk.  dMMMMMMMMMMXxkkOxdxxOOOkdolllllllllllllllllllll"
+ui_print "lllllllllllllllllllllxkOOOOOOOOOkd;kMMMMMMMMMd; lkOOOOOOOOOkxlllllllllllllllllllll"
+ui_print "lllllllllllllllllllllkkOOOOOOOOOOOxl:WMMMNc:;   lkOOOOOkxdooolllllllllllllllllllll"
+ui_print "llllllllllllllllllllloodxxkkkOOOOko' .....    .'lxkkkdocllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllcccoooll:cllllllllll:',;:::::lllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllllllc:;:llllllllllllll:;;;;lllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllcclllllllllllllclllllcllllllllllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllll. llllllllllll: :lll' llllllllllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllll. lllll;..,ll,. :ll.  lll...ll:..,llllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllll. llll .l 'c ,: :; c' l,.;. l. ..,llllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllll. ..,l,'. 'l,'. :c,.. lll,  l;'..;llllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllccccclllcccllccclllcccl,..;clllcccllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"
+ui_print "llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"
+ui_print "Created by Laddge."
+ui_print
+ui_print "- Copying sounds..."
+ui_print
+mkdir -p $MODPATH/system/media/audio/ui/
+mkdir -p $MODPATH/system/product/media/audio/ui/
+for file in $(find /sdcard/UISounds/*); do
+    cp -rf $file $MODPATH/system/media/audio/ui/
+    cp -rf $file $MODPATH/system/product/media/audio/ui/
+    ui_print "- Copied $(basename $file)"
+done
+ui_print
